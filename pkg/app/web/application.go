a0c5e839dabe24f3359a9af2783c7787088f1810
ClareChu <1062186165@qq.com>
2020-11-16 16:35:07 +0800

    添加 跨集群的线

--- pkg/app/web/application.go
+++ pkg/app/web/application.go
@@ -43,9 +43,22 @@ type webApp struct {
 }
 
 func newWebApplication() *webApp {
+	irs := iris.New()
+	irs.Use(Cors)
 	return &webApp{
-		Application: iris.New(),
+		Application: irs,
 	}
+}
+
+func Cors(ctx iris.Context) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	if ctx.Request().Method == "OPTIONS" {
+		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS")
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization")
+		ctx.StatusCode(204)
+		return
+	}
+	ctx.Next()
 }
 
 // Application is the struct of web Application
